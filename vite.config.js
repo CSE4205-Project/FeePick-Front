9f3a989a8e642a733c217d693f6eda130e72bd40
Jiyeon <kimjiyeon521@gmail.com>
2024-06-13 23:30:28 +0900

    [test]proxy

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         '/api': {
             target: 'http://13.214.177.250:5001/',
             changeOrigin: true,
+            secure: false, // HTTPS에서 HTTP로 요청을 허용하는 옵션
             rewrite: (path) => path.replace(/^\/api/, '')
         }
     }
