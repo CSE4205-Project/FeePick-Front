4b497e8283dcb6e6e3899655c93ef9ad3661f2a9
Jiyeon <kimjiyeon521@gmail.com>
2024-06-13 23:28:28 +0900

    [test]proxy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
         '/api': {
             target: 'http://13.214.177.250:5001/',
             changeOrigin: true,
-            secure: false,
             rewrite: (path) => path.replace(/^\/api/, '')
         }
     }
